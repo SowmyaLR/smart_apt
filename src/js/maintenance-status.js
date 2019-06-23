01255e2a383f19c4d3059bb73f9852ade013eb0b
unknown <Ravi Kumar Karthikeyan>
2019-06-23 10:06:29 +0530

    refs status label fix.

--- src/js/maintenance-status.js
+++ src/js/maintenance-status.js
@@ -14,8 +14,11 @@ $(function() {
       var transactId = parseInt($('#transaction-id-verify-input').val());
       if(instance.summaryData(transactId)==1){
         console.log('Approved')
+        $('#alert-label')[0]['className'] = "alert alert-success ";
+        $('#alert-label')[0]['innerHTML'] = "Approved";
       }else{
-        console.log('Status pending')
+        $('#alert-label')[0]['className'] = "alert alert-warning ";
+        $('#alert-label')[0]['innerHTML'] = "Pending";
       }
     }).then(function(err,res){
       console.log(err)
