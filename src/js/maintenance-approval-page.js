d12c71ce8e214c04d706e4db838c7a0c3996b5be
unknown <Ravi Kumar Karthikeyan>
2019-06-23 09:48:24 +0530

    refs #final commit.

--- src/js/maintenance-approval-page.js
+++ src/js/maintenance-approval-page.js
@@ -1,51 +1,167 @@
-function MaintenanceApprovalPage(){
+function MaintenanceApprovalPage() {
     var demoValue = 'test';
     var self = this;
 
-    MaintenanceApprovalPage.prototype.init = function(){
-        var index = 0;
-        //$('#content')[0].className = "row content-division";
-        var cloningContentDiv = $('#content').clone();
+    MaintenanceApprovalPage.prototype.addingTrasactionContent = function (dataList, dataCount) {
         var mainContainerClone = $('#main-container');
-        cloningContentDiv[0].setAttribute('id',('content'+'_'+index));
-        cloningContentDiv[0].className = "row content-division";
-        mainContainerClone.append(cloningContentDiv);
-        this.initializeEvents();
-        //cloningContentDiv.find("input[type='checkbox']")
-    };   
+        for (var iterator = 0; iterator < dataCount; iterator++) {
+            var cloningContentDiv = $('#content').clone();
+            var checkBoxElements;
+            cloningContentDiv[0].setAttribute('id', ('content' + '_' + iterator));
+            cloningContentDiv[0].className = "row content-division";
+            checkBoxElements = cloningContentDiv.find(".input");
+            $.each(checkBoxElements, function (index) {
+                switch (checkBoxElements[index].getAttribute('content')) {
+                    case 'house-id':
+                        //checkBoxElements[index]['innerHTML'] = dataList[iterator]['house-id'];
+                        //checkBoxElements[index]['innerHTML'] = 'test';
+                        break;
+                    case 'name':
+                        //checkBoxElements[index]['innerHTML'] = dataList[iterator]['name'];
+                        //checkBoxElements[index]['innerHTML'] = 'test';
+                        break;
+                    case 'account-number':
+                        //checkBoxElements[index]['innerHTML'] = dataList[iterator]['account-number'];
+                        //checkBoxElements[index]['innerHTML'] = 'test';
+                        break;
+                    case 'transaction-id':
+                        //checkBoxElements[index]['innerHTML'] = dataList[iterator]['transaction-id'];
+                        //checkBoxElements[index]['innerHTML'] = 'test';
+                        break;
+                    case 'amount-send':
+                        //checkBoxElements[index]['innerHTML'] = dataList[iterator]['amount-send'];
+                        //checkBoxElements[index]['innerHTML'] = 'test';
+                        break;
+                    case 'approve':
+                        //checkBoxElements[index].setAttribute('indexValue', iterator);
+                        break;
+                    case 'decline':
+                        //checkBoxElements[index].setAttribute('indexValue', iterator);
+                        break;
+                    default:
+                        break;
+                }
+            });
+            cloningContentDiv.find("input[type=checkbox]");
+            mainContainerClone.append(cloningContentDiv);
+            this.initializeEvents();
+        }
+    }
 
-    MaintenanceApprovalPage.prototype.initializeEvents = function(){
-        
-        $("input[type='checkbox']").bind('click',function(event){
+    MaintenanceApprovalPage.prototype.init = function () {
+
+        //self.addingTrasactionContent(undefined, 1);
+        //cloningContentDiv.find("input[type='checkbox']")
+
+    };
+
+    MaintenanceApprovalPage.prototype.initializeEvents = function () {
+
+        $("input[type='checkbox']").bind('click', function (event) {
             var indexValue = event['currentTarget']['attributes']['indexvalue'];
             var checkBoxType = event['currentTarget']['attributes']['content'];
             var otherCheckBox = undefined;
-            if(checkBoxType.value === 'approve'){
-                otherCheckBox = $('#content_'+indexValue.value).find("input[content='decline']")[0];
-                if(otherCheckBox !== undefined && otherCheckBox.checked === false){
+            if (checkBoxType.value === 'approve') {
+                otherCheckBox = $('#content_' + indexValue.value).find("input[content='decline']")[0];
+                if (otherCheckBox !== undefined && otherCheckBox.checked === false) {
                     event['currentTarget'].checked = true;
-                }else if(otherCheckBox !== undefined){
+                } else if (otherCheckBox !== undefined) {
                     otherCheckBox.checked = false;
                     event['currentTarget'].checked = true;
-                }else{
+                } else {
                     event.preventDefault();
                 }
-            }else{
-                otherCheckBox = $('#content_'+indexValue.value).find("input[content='approve']")[0];
-                if(otherCheckBox !== undefined  && otherCheckBox.checked === false){
+            } else {
+                otherCheckBox = $('#content_' + indexValue.value).find("input[content='approve']")[0];
+                if (otherCheckBox !== undefined && otherCheckBox.checked === false) {
                     event['currentTarget'].checked = true;
-                }else if(otherCheckBox !== undefined){
+                } else if (otherCheckBox !== undefined) {
                     otherCheckBox.checked = false;
                     event['currentTarget'].checked = true;
-                }else{
+                } else {
                     event.preventDefault();
                 }
             }
         });
+
+        $('#btn-load-all').bind('click',function(event){
+            getApprovalData();
+        });
+        async function getApprovalData(){
+            //if(App.contracts.ATContract.isDeployed()){
+              //  App.contracts.ATContract.at(App.contracts.ATContract.address);
+            //}else{
+                App.contracts.ATContract.deployed().then(function(instance){
+                    instance.getApprovalData();
+                });
+            //}
+        }
+
+        $('#btn-decline-all').bind('click',function(event){
+            var declineAll = $("input[content='approve']");
+            $.each(declineAll, function(index){
+                declineAll[index].checked = false;
+            });
+            
+            var approveAll = $("input[content='decline']");
+            $.each(approveAll, function(index){
+                approveAll[index].checked = true;
+            });
+
+        });
+
+        $('#btn-approve-all').bind('click',function(event){
+            var declineAll = $("input[content='decline']");
+            $.each(declineAll, function(index){
+                declineAll[index].checked = false;
+            });
+            
+            var approveAll = $("input[content='approve']");
+            $.each(approveAll, function(index){
+                approveAll[index].checked = true;
+            });
+
+        });
     };
+
+        $('#btn-submit-all').bind('click',function(event){
+            var deferred = $.Deferred();
+            var transactionIdList = [];
+            $.Deferred().resolve().then(function(){
+                return self.getSubmittedData();
+                //call your save method
+                //return deferred.resolve();
+            }).then(function(result){
+                transactionIdList = result;
+                console.log(transactionIdList);
+                return deferred.resolve();
+            }).fail(function(){
+                return deferred.reject();
+            });
+            return deferred.promise();
+        });
+    
+
+        MaintenanceApprovalPage.prototype.getSubmittedData = function(){
+            var transactionList = [];
+            var approvedFrame = $("input[content='approve']");
+            var deferred = $.Deferred();
+            $.Deferred().resolve().then(function(){
+                $.each(approvedFrame, function(index){
+                    if(approvedFrame[index].checked){
+                        transactionList.push($(".input[content='transaction-id']")[index].innerHTML);
+                    }
+                });
+            }).then(function(){
+                return deferred.resolve(transactionList);
+            }).fail(function(){
+                return deferred.reject();
+            });
+            return deferred.promise();
+    }
 }
 
-$(function(){
+$(function () {
     var maintenanceApprovalPage = new MaintenanceApprovalPage();
     maintenanceApprovalPage.init();
 });
--- src/js/maintenance-report-generate.js
+++ src/js/maintenance-report-generate.js
@@ -1,24 +1,34 @@
 
 function MaintenanceReportGenerator(tokenValue) {
     console.log(tokenValue);
-    this.initializeEvents();
-};
-
-MaintenanceReportGenerator.prototype.initializeEvents = function(){
     var self = this;
-    $("#btn-report-generate").bind("click",function(){
-        alert("Given paragraph was clicked."); 
-        self.downloadPDF();
-    });
-};
 
-MaintenanceReportGenerator.prototype.downloadPDF = function(){
-    var js =  new jsPDF();
-    js.fromHTML($('#PDFcontent').html());
-    js.save('value.pdf');
-};
+    MaintenanceReportGenerator.prototype.iterateContent = function(){
+        var mainDiv = $('#report-sheet');
+        for(var iterator=0;iterator < 100;iterator++){
+            var clonedContendts = $('#PDFcontent').clone();
+            mainDiv.append(clonedContendts);
+        }
+        self.initializeEvents();
+    };
 
+    MaintenanceReportGenerator.prototype.initializeEvents = function () {
+        $("#btn-report-generate").bind("click", function () {
+            alert("Given paragraph was clicked.");
+            self.downloadPDF();
+        });
+    };
+
+    MaintenanceReportGenerator.prototype.downloadPDF = function () {
+        var js = new jsPDF();
+        js.fromHTML($('#report-sheet').html());
+        js.save('value.pdf');
+    };
+
+};
 
 $(function () {
     var appFunction = new MaintenanceReportGenerator('token');
+    appFunction.iterateContent();
+    
 });
